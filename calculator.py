49127e7d66f2bd0442ffb1fc31139f545856eb20
Asu144 <asupatel2007@gmail.com>
2026-07-10 21:03:30 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,16 @@
+# calculator.py
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Sum:", a + b)
+print("Difference:", a - b)
+print("Product:", a * b)
+print("Quotient:", a / b)
+
+
+#calculation with decimals..
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+print("Sum:", a + b)
+print("Difference:", a - b)
+print("Product:", a * b)
+print("Quotient:", a / b)
