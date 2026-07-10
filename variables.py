b1b74e0763f54f8f6dc714ffb798bae4bff9da84
Asu144 <asupatel2007@gmail.com>
2026-07-10 20:51:34 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,8 @@
+# variables.py
+age = 25
+height = 5.9
+name = "Alex"
+is_student = True
+print(type(age), type(height), type(name), type(is_student)).
+
+#<class 'int'> <class 'float'> <class 'str'> <class 'bool'>
